1dddf18f690272cfbce50aa78cc68dc8d2b84659
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2022-06-27 12:19:20 -0400

    use dev

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 
 
 # this was tested on an t2.small instance running Ubuntu 22.04 LTS (GNU/Linux 5.15.0-1011-aws x86_64)
-# 8 GB disk space
+# 8 GB disk space / I would recommend slightly more (can run out of space when building the docker images)
 
 # add secrets folder to datacrossways folder, and add config.json
 sudo apt-get update
