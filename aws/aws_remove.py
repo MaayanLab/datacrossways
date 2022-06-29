8e9c54b0796cd82a6c1f08afcb1e605bf2d4f85c
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2022-06-29 17:17:43 -0400

    aws config

---
+++ aws/aws_remove.py
@@ -0,0 +1,117 @@
+import sys
+import boto3
+import json
+from tqdm import trange
+import time
+import sys
+
+u_key = sys.argv[1]
+u_secret = sys.argv[2]
+project_name = sys.argv[3]
+
+aws_del = {}
+try:
+    with open("aws_config_data.json", "r") as f:
+        aws_del = json.load(f)
+except Exception:
+    print("AWS resource file could not be read")
+    quit()
+
+def delete_bucket_completely(s3, bucket_name):
+    response = s3.list_objects_v2(
+        Bucket=bucket_name,
+    )
+    while response['KeyCount'] > 0:
+        print('Deleting %d objects from bucket %s' % (len(response['Contents']),bucket_name))
+        response = s3.delete_objects(
+            Bucket=bucket_name,
+            Delete={
+                'Objects':[{'Key':obj['Key']} for obj in response['Contents']]
+            }
+        )
+        response = s3.list_objects_v2(
+            Bucket=bucket_name,
+        )
+    print('Now deleting bucket %s' % bucket_name)
+    response = s3.delete_bucket(
+        Bucket=bucket_name
+    )
+
+def delete_all(iam, s3, aws_del):
+    counter = 0
+    error_counter = 0
+    try:
+        response = iam.detach_user_policy(
+            UserName=aws_del["user"]["UserName"],
+            PolicyArn=aws_del["policy"]["Arn"]
+        )
+        counter = counter+1
+        print(colored(0,255,0, " - policy detached"))
+    except Exception:
+        print(colored(255,255,0, " - policy could not be detached"))
+        error_counter = error_counter+1
+
+    try:
+        respsone = iam.delete_policy(PolicyArn=aws_del["policy"]["Arn"])
+        counter = counter+1
+        print(colored(0,255,0, " - policy deleted"))
+    except Exception:
+        print(colored(255,255,0, " - policy could not be deleted"))
+        error_counter = error_counter+1
+    
+    try:
+        response = iam.delete_access_key(
+            AccessKeyId=aws_del["user"]["key"]["AccessKeyId"],
+            UserName=aws_del["user"]["UserName"]
+        )
+        counter = counter+1
+        print(colored(0,255,0, " - user access key deleted"))
+    except Exception:
+        print(colored(255,255,0," - user access key could not be deleted"))
+        error_counter = error_counter+1
+    
+    try:
+        respsone = iam.delete_user(UserName=aws_del["user"]["UserName"])
+        counter = counter+1
+        print(colored(0,255,0, " - user deleted"))
+    except Exception:
+        print(colored(255,255,0," - user could not be deleted"))
+        error_counter = error_counter+1
+    
+    try:
+        response = delete_bucket_completely(s3, aws_del["bucket"]["Location"].replace("/",""))
+        counter = counter+1
+        print(colored(0,255,0, " - S3 bucket deleted"))
+    except Exception:
+        print(colored(255,255,0," - S3 bucket could not be deleted"))
+        error_counter = error_counter+1
+    
+    print("\nScript completed")
+    if error_counter > 0:
+        print(colored(255,0,0, "The script encountered "+str(error_counter)+" errors. Some of the resources might not have been removed or have been removed previously."))
+
+def colored(r, g, b, text):
+    return "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, text)
+
+print("-"*80)
+print(colored(255, 0, 0, "\033[1mWARNING\033[0m") + "Deleting project: \033[1m"+project_name+"\033[0m\n")
+print(colored(255, 0, 0, "!!! You are about to \033[1mpermanently\033[0m")+colored(255,0,0, "remove all data associated with the project !!!\n"))
+print(colored(255, 255, 0, "The script will delete"))
+print(colored(255, 255, 0, " - AWS user:\t")+aws_del["user"]["UserName"])
+print(colored(255, 255, 0, " - S3 bucket:\t")+aws_del["bucket"]["Location"].replace("/", ""))
+print(colored(255, 255, 0, " - RDS database: "))
+print("-"*80)
+val = input("Remove all resources on AWS (Y/n)? : ")
+if val == "Y":
+    val = input("Re-enter name of project to delete all accociated resources: ")
+    if val == project_name:
+        for i in trange(100, desc="Countdown ", bar_format="{desc:<5} |{bar}", leave=False):
+            time.sleep(0.02)
+        print("Deleting resources.\n")
+        iam = boto3.client("iam",
+                aws_access_key_id=u_key,
+                aws_secret_access_key=u_secret)
+        s3 = boto3.client("s3",
+                aws_access_key_id=u_key,
+                aws_secret_access_key=u_secret)
+        delete_all(iam, s3, aws_del)
---
+++ aws/aws_setup.py
@@ -0,0 +1,141 @@
+#!/usr/bin/python
+
+# this script will create a new bucket and add a user that can interact with it
+import sys
+import boto3
+import json
+
+u_key = sys.argv[1]
+u_secret = sys.argv[2]
+project_name = sys.argv[3]
+
+if "_" in project_name:
+    print("Project name cannot contain underscores.")
+elif " " in project_name:
+    print("Project name cannot contain spaces.")
+elif "." in project_name:
+    print("Project name cannot contain periods.")
+elif "," in project_name:
+    print("Project name cannot contain commas.")
+else:
+
+    aws_resources = {}
+
+    iam = boto3.client("iam",
+                    aws_access_key_id=u_key,
+                    aws_secret_access_key=u_secret)
+
+    s3 = boto3.client("s3",
+                    aws_access_key_id=u_key,
+                    aws_secret_access_key=u_secret)
+    
+    def colored(r, g, b, text):
+        return "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, text)
+
+    def list_users(iam):
+        paginator = iam.get_paginator('list_users')
+        for response in paginator.paginate():
+            for user in response["Users"]:
+                print(f"Username: {user['UserName']}, Arn: {user['Arn']}")
+
+    def create_policy(iam, project_name):
+        f = open("bucket_policy_template.json")
+        policy = json.load(f)
+        f.close()
+        for s in policy["Statement"]:
+            s["Resource"][0] = "arn:aws:s3:::"+project_name+"-vault/*"
+        response = iam.create_policy(
+            PolicyName=project_name+"-bucket-permission",
+            PolicyDocument=json.dumps(policy)
+        )
+        return(response)
+
+    def create_bucket(s3, project_name):
+        bucket_name = (project_name+"-vault").replace("_", "-")
+        return(s3.create_bucket(Bucket=bucket_name))
+
+    def create_user(iam, project_name):
+        response = iam.create_user(UserName=project_name+"-user")
+        return(response)
+
+    def attach_user_policy(iam, policy_arn, username):
+        response = iam.attach_user_policy(
+            UserName=username,
+            PolicyArn=policy_arn
+        )
+        print(response)
+
+    def create_access_key(iam, username):
+        response = iam.create_access_key(
+            UserName=username
+        )
+        return(response['AccessKey'])
+
+    def attach_cors(s3, bucket):
+        f = open("bucket_cors_template.json")
+        cors_configuration = json.load(f)
+        f.close()
+        response = s3.put_bucket_cors(Bucket=bucket,
+                    CORSConfiguration=cors_configuration)
+        print(response)
+
+    def block_bucket(s3, bucket):
+        response = s3.put_public_access_block(
+                    Bucket=bucket,
+                    PublicAccessBlockConfiguration={
+                        'BlockPublicAcls': True,
+                        'IgnorePublicAcls': True,
+                        'BlockPublicPolicy': True,
+                        'RestrictPublicBuckets': True
+                    }
+                )
+        return(response)
+
+    try:
+        user = create_user(iam, project_name)
+        aws_resources["user"] = user["User"]
+        print(colored(0,255,0," - user created"))
+    except Exception:
+        print(colored(255,255,0," - user already exists"))
+
+    try:
+        policy = create_policy(iam, project_name)
+        aws_resources["policy"] = policy["Policy"]
+        print(colored(0,255,0," - policy created"))
+    except Exception:
+        print(colored(255,255,0," - policy could not be created"))
+
+    try:
+        key = create_access_key(iam, aws_resources["user"]["UserName"])
+        aws_resources["user"]["key"] = key
+        print(colored(0,255,0," - user access key created"))
+    except Exception:
+        print(colored(255,255,0," - user access key could not be created"))
+
+    try:
+        bucket = create_bucket(s3, project_name)
+        aws_resources["bucket"] = bucket
+        print(colored(0,255,0," - bucket created"))
+    except Exception:
+        print(colored(255,255,0," - S3 bucket could not be created"))
+
+    try:
+        attach_user_policy(iam, aws_resources["policy"]["Arn"], aws_resources["user"]["UserName"])
+        print(colored(0,255,0," - policy attached to user"))
+    except Exception:
+        print(colored(255,255,0," - upolicy could not be attached to user"))
+
+    try:
+        attach_cors(s3, aws_resources["bucket"]["Location"].replace("/",""))
+        print(colored(0,255,0," - CORS rules attached to S3 bucket"))
+    except Exception:
+        print(colored(255,255,0," - CORS rules could not be attached to S3 bucket"))
+
+    try:
+        block_bucket(s3, aws_resources["bucket"]["Location"].replace("/",""))
+        print(colored(0,255,0," - bucket privacy enhanced"))
+    except Exception:
+        print(colored(255,255,0," - S3 bucket privacy could not be enhanced"))
+
+    with open('aws_config_'+project_name+'.json', 'w') as f:
+        f.write(json.dumps(aws_resources, indent=4, sort_keys=True, default=str))
