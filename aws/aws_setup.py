7f4c9489c3b1ad42b161d5795017a9dbdec4d80e
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2022-06-30 21:48:41 -0400

    Update aws_setup.py

--- aws/aws_setup.py
+++ aws/aws_setup.py
@@ -139,7 +139,7 @@ else:
         console.print(" :thumbs_up: policy created", style="green")
     except Exception as err:
         print(colored(255,255,0," x policy could not be created"))
-        console.print(str(err))
+        console.print(str(err).replace("An error occurred ", "").replace("(", "").replace(")", ""))
 
     try:
         key = create_access_key(iam, aws_resources["user"]["UserName"])
