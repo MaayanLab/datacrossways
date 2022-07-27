f110c846ee5497adafab82099aee3271a98722c3
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2022-07-27 17:26:55 -0400

    add env

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo What is the domain name (e.g. datacommons.org)?
+read domain
+
+echo What is the email address for Let\'s Encrypt notifications (e.g. myemail@admin.org)?
+read email
+
+#docker compose up
