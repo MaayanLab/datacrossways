b772178324f22c1b02df7c5f66f1569f39e70ba9
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2022-07-27 18:29:04 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ echo What is the domain name \(e.g. datacommons.org\)?
 read -p 'Domain: ' domain
 
 echo What is the email address for Let\'s Encrypt notifications \(e.g. myemail@admin.org\)?
-read -p 'E-mail:' email
+read -p 'E-mail: ' email
 
 export BASE_URL=$domain
 export LETS_ENCRYPT_EMAIL=$email
