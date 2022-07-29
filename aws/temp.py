7fc856521e6684ad1477641368d8759cd49f917c
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2022-07-28 21:12:57 -0400

    complete

---
+++ aws/temp.py
@@ -0,0 +1,5 @@
+aws_id = input("Enter AWS id: ")
+aws_key = input("Enter AWS key: ")
+project_name = input("Enter project name: ")
+admin_email = imput("Enter admin email (Google OAuth): ")
+
--- complete_setup.sh
+++ complete_setup.sh
@@ -1,3 +1,7 @@
-path=$(pwd)
-echo $path
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+echo $SCRIPT_DIR
+cd $SCRIPT_DIR
+
+git clone https://github.com/MaayanLab/datacrossways_api.git
