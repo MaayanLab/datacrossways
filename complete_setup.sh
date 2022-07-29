d27b31bfcadbf315dd75414993c2e51d386cd533
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2022-07-28 22:10:27 -0400

    Update complete_setup.sh

--- complete_setup.sh
+++ complete_setup.sh
@@ -7,7 +7,6 @@ cd $SCRIPT_DIR
 rm -rf datacrossways_api
 git clone https://github.com/MaayanLab/datacrossways_api.git
 
-pip install SQLAlchemy==1.4.39
-pip install Flask_SQLAlchemy==2.5.1
+pip install -r requirements.txt
 
 python3 datacrossways_api/createdb.py
