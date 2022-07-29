6c424c49bc2a2994a3ed5ba57f16264988516937
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2022-07-28 23:08:16 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,10 @@
 # this was tested on an t2.small instance running Ubuntu 22.04 LTS (GNU/Linux 5.15.0-1011-aws x86_64)
 # 8 GB disk space / I would recommend slightly more (can run out of space when building the docker images)
 
+AWS_ID=$1
+AWS_KEY=$2
+PROJECT_NAME=$3
+
 # add secrets folder to datacrossways folder, and add config.json
 OLD_DIR=$(pwd)
 SCRIPT_DIR=$( dirname -- "$0"; )
