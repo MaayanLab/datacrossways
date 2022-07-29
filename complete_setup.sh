e00e75a8fa07c6fb5dd647afc94201c30ec72917
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2022-07-28 22:05:28 -0400

    Update complete_setup.sh

--- complete_setup.sh
+++ complete_setup.sh
@@ -4,4 +4,7 @@ SCRIPT_DIR=$( dirname -- "$0"; )
 echo $SCRIPT_DIR
 cd $SCRIPT_DIR
 
+rm -rf datacrossways_api
 git clone https://github.com/MaayanLab/datacrossways_api.git
+
+python3 datacrossways_api/createdb.py
